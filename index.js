af877ceb261bca7dfed60b99b44084aaff504a34
Sanjeet Pathak <sanjeet.pathak990@gmail.com>
2019-06-23 17:55:59 +0530

    Changes

--- index.js
+++ index.js
@@ -34,30 +34,6 @@ db.settings({timestampsInSnapshots: true});
 
 var port = process.env.PORT || 3000;
 
-
-
-app.onExecute((body, headers) => {
-  // TODO Send command to device
-  return {
-    requestId: body.requestId,
-    payload: {
-      commands: [{
-        ids: ["123"],
-        status: "SUCCESS",
-        states: {
-          on: true,
-          online: true
-        }
-      }, {
-        ids: ["456"],
-        status: "ERROR",
-        errorCode: "deviceTurnedOff"
-      }]
-    }
-  };
-});
-
-
 app.onSync(async (body, headers) => {
   const userEmail = await getEmail(headers);
   const userDoc = await db.collection('users').doc(userEmail).get();
@@ -116,5 +92,87 @@ app.onSync(async (body, headers) => {
     }
   }
 });
+
+
+app.onExecute(async (body, headers) => {
+  // TODO Send command to device
+
+  const userId = await getEmail(headers);
+  
+  const commands = [{
+    ids: [],
+    status: 'SUCCESS',
+    states: {},
+  }];
+  
+  const { devices, execution } = body.inputs[0].payload.commands[0];
+  
+  devices.forEach(device => {
+	  try {
+			const states = doExecute(userId, device.id, execution[0]);
+			commands[0].ids.push(device.id);
+			commands[0].states = states;
+			// Report state back to Homegraph
+			app.reportState({
+				agentUserId: userId,
+				requestId: Math.random().toString(),
+				payload: {
+					devices: {
+						states: {
+							[device.id]: states,
+						},
+					},
+				},
+			});
+		}
+		catch (e) {
+			commands.push({
+				ids: [device.id],
+				status: 'ERROR',
+				errorCode: e.message,
+			});
+		}
+  });
+  
+  return {
+        requestId: body.requestId,
+        payload: {
+            commands,
+        },
+    };
+});
+
+app.onDisconnect((body, headers) => {
+  // TODO Disconnect user account from Google Assistant
+  // You can return an empty body
+  return {};
+});
+
+const doExecute = async (userId, deviceId, execution) => {
+        const doc = await db.collection('users').doc(userId).collection('devices').doc(deviceId).get();
+        if (!doc.exists) {
+            throw new Error('deviceNotFound' + deviceId);
+        }
+        const states = {
+            online: true,
+        };
+        const data = doc.data();
+        if (!data.states.online) {
+            throw new Error('deviceOffline');
+        }
+        switch (execution.command) {
+            // action.devices.traits.ArmDisarm
+            case 'action.devices.commands.OnOff':
+                await db.collection('users').doc(userId).collection('devices').doc(deviceId).update({
+                    'states.on': execution.params.on,
+                });
+                states['on'] = execution.params.on;
+                break;
+            // action.devices.traits.OpenClose
+            default:
+                throw new Error('actionNotAvailable');
+        }
+        return states;
+}
 
 express().use(bodyParser.json(), app).listen(port);
