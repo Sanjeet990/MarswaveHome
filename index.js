8fcdcf94a5a05a25cb11b18041a36212646b882d
Sanjeet Pathak <sanjeet.pathak990@gmail.com>
2019-06-23 17:07:45 +0530

    Changes

--- index.js
+++ index.js
@@ -94,8 +94,51 @@ app.onSync(async (body, headers) => {
 });
 
 
-app.onExecute(async (body, headers) => {
-
+app.onExecute((body, headers) => {
+  // TODO Send command to device
+  const userId = getEmail(headers);
+  
+  const commands = [{
+    ids: [],
+    status: 'SUCCESS',
+    states: {},
+  }];
+  
+  const { devices, execution } = body.inputs[0].payload.commands[0];
+  
+  devices.forEach(device => {
+	  try {
+			//const states = doExecute(userId, device.id, execution[0]);
+			commands[0].ids.push(device.id);
+			commands[0].states = states;
+			// Report state back to Homegraph
+			app.reportState({
+				agentUserId: userId,
+				requestId: Math.random().toString(),
+				payload: {
+					devices: {
+						states: {
+							[device.id]: states,
+						},
+					},
+				},
+			});
+		}
+		catch (e) {
+			commands.push({
+				ids: [device.id],
+				status: 'ERROR',
+				errorCode: e.message,
+			});
+		}
+  });
+  
+  return {
+        requestId: body.requestId,
+        payload: {
+            commands,
+        },
+    };
 });
 
 app.onDisconnect((body, headers) => {
